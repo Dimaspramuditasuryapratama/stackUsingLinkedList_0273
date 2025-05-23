ec143c0e39b45bc8579dd643966f962199fad38c
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-23 08:22:00 +0700

    Add switch case for push operation in main menu

--- file.cpp
+++ file.cpp
@@ -92,4 +92,13 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
     }
+
+    switch (choice)
+    {
+        case 1:
+            cout << "Enter value to push: ";
+            cin >> value;
+            stack.push(value); //push the entered value onto the stack
+            break;
+    }
 }
