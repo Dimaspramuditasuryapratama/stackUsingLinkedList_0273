c2c5f64501672e4ca3ad6f1620b2e2a9eb179774
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-23 07:53:34 +0700

    Implement stack class with push operation

--- file.cpp
+++ file.cpp
@@ -14,3 +14,25 @@ class Node
         }
 };
 
+// stack class
+class stack
+{
+    private:
+        Node *top; //pointer to the top node of the stack
+    
+    public:
+        stack()
+        {
+            top = NULL; //initialize the stack with a null top pointer
+        }
+        // push operation: insert an element onto the top of the stack
+        int push(int value)
+        {
+            Node *newNode = new Node(); //Allocate memory for the  new node
+            newNode->data = value; //assign value
+            newNode->next = top; //set the next pointer of the new node to the current top
+            top = newNode; //update the top pointer to point to the new node
+            cout << "Pushed " << value << endl;
+            return 0;
+        }
+};
